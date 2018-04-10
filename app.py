fdb06dafc3626a85600707fa797848a63ca60b6e
Advisedin <vahid_milani@yahoo.com>
2018-04-10 23:19:05 +1000

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# -*- coding:utf8 -*-
+# !/usr/bin/env python
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from __future__ import print_function
 from future.standard_library import install_aliases
@@ -40,9 +55,8 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-	
-    ##res = processRequest(req)
-	res=makeWebhookResult1(req)
+    res = processRequest(req)
+
     res = json.dumps(res, indent=4)
     # print(res)
     r = make_response(res)
@@ -60,8 +74,7 @@ def processRequest(req):
     yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
     result = urlopen(yql_url).read()
     data = json.loads(result)
-    ##res = makeWebhookResult(data)
-	makeWebhookResult()
+    res = makeWebhookResult(data)
     return res
 
 
@@ -75,75 +88,6 @@ def makeYqlQuery(req):
     return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
 
-
-def makeWebhookResult1(req):
-	if req.get("result").get("action") != "yahooWeatherForecast":
-        return {}
-    query = data.get('query')
-    if query is None:
-        return {}
-
-    result = query.get('results')
-    if result is None:
-        return {}
-
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
-
-    # print(json.dumps(item, indent=4))
-  # Vahid Test Begin
-    num1 = 1
-    num2 = 6
-    
-    #sum = (num1) + (num2)
-    # File Read Begin
-    ##f = open('text.txt','r')
-    ##lines = f.readlines()
-    ##sum = len(lines)
-    ##f.close()
-    
-    list1 = ['larry', 'curly', 'moe']
-    list2 = ['3.50', '4.0', '5.30']
-    x= list1.index('moe')
-    sum= list2[x]
-    
-    
-    # File Read Ends
- # Vahid Test End
-    speech = str(sum)
-    ##speech = sum
-    
-    #"Today the weather in " + location.get('city') + ": " + condition.get('text') + \
-             #", And the temperature is " + condition.get('temp') + " " + units.get('temperature')
-
-    print("Response:")
-    #print(speech)
-    #VM
-    #file = open(“requirements.txt”, “r”) 
-    
-    
-    #print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
-
-    return {
-        
-        "speech": speech,
-        "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
-    }
-	
 def makeWebhookResult(data):
     query = data.get('query')
     if query is None:
@@ -215,5 +159,7 @@ if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
     print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0')
 
     app.run(debug=False, port=port, host='0.0.0.0')
